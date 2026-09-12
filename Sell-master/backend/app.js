f6cd01f49a2b15ac149e468accf997079b0eb7e8
muhammadkashanali <kashanalighouri40@gmail.com>
2026-09-12 13:44:23 +0500

    Mark catalog seeding disabled

--- Sell-master/backend/app.js
+++ Sell-master/backend/app.js
@@ -32,6 +32,7 @@ app.get("/api/health", (_req, res) => {
   res.status(200).json({
     ok: true,
     message: "Pacxone API is running.",
+    catalogSeeding: false,
   });
 });
 
@@ -39,6 +40,7 @@ app.get("/", (_req, res) => {
   res.status(200).json({
     ok: true,
     message: "Pacxone API is running.",
+    catalogSeeding: false,
   });
 });
 
